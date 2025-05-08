59ae2816426eae5b746d1e26a36aec6a57452b9d
DavidIsrael <davidisrael.rj@gmail.com>
2025-05-07 22:35:44 -0300

    Teste Drawer

--- App.jsx
+++ App.jsx
@@ -1,31 +1,45 @@
+import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-// import { Icon } from "react-native-vector-icons/MaterialIcons";
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import Drawer from "./src/navegacao/Drawer";
-import { enableScreens } from "react-native-screens";
+import { Button, Text, View } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { useNavigation } from '@react-navigation/native';
 
-export default function App(){
-  enableScreens();
-  return(
-    <SafeAreaView>
-      <NavigationContainer>
-        <Drawer/>
-      </NavigationContainer>
-    </SafeAreaView>
-    // <View style={styles.container}>
-    //   <Text>Compras</Text>
-    //   <Icon name={"emoticon-cool"} size={150} color={"#000"}/>
-    // </View>
-  )
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Profile" component={ProfileScreen} />
+    </Drawer.Navigator>
+  );
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:"center",
-    alignItems: "center",
-    backgroundColor:"#FFF",
+function HomeScreen() {
+  const navigation = useNavigation();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Profile')} title='Go to Profile '/> 
+        
+    </View>
+  );
+}
 
-  }
-})
+function ProfileScreen() {
+  const navigation = useNavigation();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+      <Button onPress={() => navigation.navigate('Home')} title='Go to Home'/>
+    </View>
+  );
+}
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <MyDrawer/>
+    </NavigationContainer>
+  )
+}
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,24 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+// /**
+//  * Metro configuration
+//  * https://reactnative.dev/docs/metro
+//  *
+//  * @type {import('@react-native/metro-config').MetroConfig}
+//  */
+// const config = {};
+
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const config = {
+  resolver: {
+    extraNodeModules: {
+      'react-native-gesture-handler': require.resolve('react-native-gesture-handler'),
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
