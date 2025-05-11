e3c79fe0beb8f6ad3226cde304fadb8e7aed75e4
DavidIsrael <davidisrael.rj@gmail.com>
2025-05-10 23:15:57 -0300

    Personalizando o menu do DrawerItem

--- src/components/ComponDrawer.jsx
+++ src/components/ComponDrawer.jsx
@@ -1,12 +1,36 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
 import TelaA from "../views/TelaA";
 import TelaB from "../views/TelaB";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { useNavigation } from '@react-navigation/native';
 
 const Drawer = createDrawerNavigator();
 
+const CustomDrawerContent = () => {
+  
+  const navigation = useNavigation();
+
+  return (
+    <DrawerContentScrollView>
+      <DrawerItem
+        icon={({ color }) => <Icon size={28} name={"account"} color={color} />}
+        label="TelaA"
+        onPress={() => navigation.navigate('TelaA')}
+      />
+      <DrawerItem
+        icon={({ color }) => <Icon size={28} name={"apple"} color={color} />}
+        label="TelaB"
+        onPress={() => navigation.navigate('TelaB')}
+      />
+    </DrawerContentScrollView>
+  )
+}
+
+
 export default function ComponDrawer() {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      drawerContent={() => <CustomDrawerContent />}>
       <Drawer.Screen name="TelaA" component={TelaA} />
       <Drawer.Screen name="TelaB" component={TelaB} />
     </Drawer.Navigator>
--- src/views/TelaA.jsx
+++ src/views/TelaA.jsx
@@ -1,12 +1,14 @@
 import { useNavigation } from '@react-navigation/native';
 import React from "react";
 import { Button, Text, View } from 'react-native';
+import  Icon  from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function TelaA() {
     const navigation = useNavigation();
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>TelaA</Text>
+      <Icon name={'abacus'} size={30} color={'#000'}/>
       <Button onPress={() => navigation.navigate('TelaB')} title='Ir para Tela B '/> 
         
     </View>
