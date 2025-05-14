0d102d6a81ea0e08d6be7cb0e28377dcb128f27d
DavidIsrael <davidisrael.rj@gmail.com>
2025-05-14 18:36:03 -0300

    Adicionando icones no tabBarIcon

--- src/components/ComponTabs.jsx
+++ src/components/ComponTabs.jsx
@@ -3,14 +3,41 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import TelaA from "../views/TelaA"
 import TelaB from "../views/TelaB"
 import ComponStack from "./ComponStack"
+import Icon from "react-native-vector-icons/MaterialCommunityIcons"
 
 const Tab = createBottomTabNavigator()
 
-export default function ComponTabs(){
-    return(
-        <Tab.Navigator screenOptions={{headerShown: false }}> 
-            <Tab.Screen name="HomeStack" component={ComponStack}/>
-            <Tab.Screen name="TelaB" component={TelaB}/>
+export default function ComponTabs() {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarShowLabel: true,
+                tabBarActiveTintColor: '#4f4f4f',
+                tabBarInactiveTintColor: '#c0c0c0',
+                tabBarStyle: { height: 60, width: '100%' },
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    switch (route.name) {
+                        case 'HomeStack':
+                            iconName = focused
+                                ? 'account-details'
+                                : 'account-details-outline'
+                            break;
+                        case 'TelaB':
+                            iconName = focused
+                                ? 'account-edit'
+                                : 'account-edit-outline'
+                            break;
+                    }
+                    return <Icon name={iconName} size={size} color={color} />
+
+                },
+            })}
+
+        >
+            <Tab.Screen name="HomeStack" component={ComponStack} options={{tabBarLabel: 'HomeStack'}} />
+            <Tab.Screen name="TelaB" component={TelaB} options={{tabBarLabel: 'TelaB'}}/>
         </Tab.Navigator>
     )
 }
