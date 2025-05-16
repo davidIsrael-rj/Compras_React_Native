1442c2dfc781b25736b52d2dfe8d8d90c7e21375
DavidIsrael <davidisrael.rj@gmail.com>
2025-05-16 00:06:40 -0300

    Criando um card de produto

--- src/views/TelaA.jsx
+++ src/views/TelaA.jsx
@@ -3,11 +3,17 @@ import { useNavigation } from '@react-navigation/native';
 import React from "react";
 import { Button, Text, View } from 'react-native';
 import  Icon  from 'react-native-vector-icons/MaterialCommunityIcons';
+import Produto from '../components/Produto';
 
+export const imagens ={
+  
+  mouse: require('../imgs/mouse.jpg')
+}
 export default function TelaA() {
     const navigation = useNavigation();
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Produto nome="Caderno" preco="2.50" uri="mouse"/>
       <Text>TelaA</Text>
       <Icon name={'abacus'} size={30} color={'#000'}/>
       <Button onPress={() => navigation.navigate('TelaB')} title='Ir para Tela B '/> 
