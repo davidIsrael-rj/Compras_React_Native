9861bcfd10db887f757db1066430169567cc599e
DavidIsrael <davidisrael.rj@gmail.com>
2025-05-28 22:32:23 -0300

    Pedindo permissão para utilizar a câmera

--- src/views/TelaB.jsx
+++ src/views/TelaB.jsx
@@ -1,31 +1,61 @@
 import { useNavigation } from "@react-navigation/native";
-import React, { useState } from "react";
-import { Button, Image, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Alert, Button, Image, Text, TouchableOpacity, View } from "react-native";
 import { launchCamera } from "react-native-image-picker";
+import { check, PERMISSIONS, request } from "react-native-permissions";
 
 export default function TelaB() {
   const navigation = useNavigation();
   const [imagem, setImagem] = useState(null);
+  const [permissaoConcedida, setPermissaoConcedida] = useState(false);
 
-  const abrirCamera = async () => {
-    const options = {
-      mediaType: 'photo',
-      quality: 1,
-    };
-
-    launchCamera(options, (response) => {
-      if (response.assets && response.assets.length > 0) {
-        setImagem(response.assets[0].uri);
-      }
-    });
+  
+  useEffect(() => {
+    verificarPermissao();
+  }, []);
+  
+  const verificarPermissao = async () => {
+    const permissao = await check(PERMISSIONS.ANDROID.CAMERA);
+    if (permissao === 'granted') {
+      setPermissaoConcedida(true);
+    } else {
+      solicitarPermissao();
+    }
   };
+  
+  const solicitarPermissao = async () => {
+    const resultado = await request(PERMISSIONS.ANDROID.CAMERA);
+    if (resultado === 'granted') {
+      setPermissaoConcedida(true);
+    }
+  };
+  
+  const abrirCamera = async () => {
+    if (permissaoConcedida) {
+      const options = {
+        mediaType: 'photo',
+        quality: 1,
+      };
+
+      launchCamera(options, (response) => {
+        if (response.assets && response.assets.length > 0) {
+          setImagem(response.assets[0].uri);
+        }
+      });
+    } else {
+      // console.warn('Permissão não concedida');
+      Alert.alert("Permissão não concedida")
+
+    }
+  };
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Profile Screen</Text>
       <Button onPress={() => navigation.navigate('TelaA')} title='Ir para Tela A' />
 
       <TouchableOpacity onPress={abrirCamera}>
-        <View style={{ backgroundColor: '#007bff', padding: 10, borderRadius: 5, margin:10 }}>
+        <View style={{ backgroundColor: '#007bff', padding: 10, borderRadius: 5, margin: 10 }}>
           <Text style={{ color: '#FFFFFF', fontSize: 16 }}>Abrir Câmera</Text>
         </View>
       </TouchableOpacity>
