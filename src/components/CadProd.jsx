f5d288ae8fb090759d218786b3bc261b1a1aa37a
DavidIsrael <davidisrael.rj@gmail.com>
2025-06-02 19:58:32 -0300

    Estilizando o campo

--- src/components/CadProd.jsx
+++ src/components/CadProd.jsx
@@ -1,10 +1,36 @@
-import { Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function CadProd(){
-    return(
-        <View>
-            <Text>Nome</Text>
-            <TextInput/>
+export default function CadProd() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.campo}>
+                <Text style={styles.rotulo}>Nome</Text>
+                <TextInput style={styles.inptText} />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#d3d3d3"
+    },
+    campo: {
+        padding: 15,
+    },
+    rotulo: {
+        position: "absolute",
+        left: 30,
+        top:5,
+        paddingHorizontal:5,
+        backgroundColor: "#d3d3d3",
+        zIndex:1
+    },
+    inptText: {
+        borderColor: "#000",
+        backgroundColor:"#d3d3d3",
+        borderWidth: 2,
+        borderRadius: 15
+    },
+})
